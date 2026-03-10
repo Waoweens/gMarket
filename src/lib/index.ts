395bddce019ff2ea77fb74f79d87e606b6c99940
Waoweens <30313089+Waoweens@users.noreply.github.com>
2026-03-10 17:42:48 +0700

    empty user object

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,15 @@
+import type { User } from "./server/db/schema";
 import SnowflakeGenerator from "./snowflake";
 
 // place files you want to import through the `$lib` alias in this folder.
 export const snowflake = new SnowflakeGenerator(67);
+
+export const emptyUser = (): User => {
+	return {
+		id: 0n,
+		username: '',
+		passwordHash: '',
+		displayName: '',
+		bio: ''
+	}
+}
