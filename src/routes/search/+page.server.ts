49f985283ccfe96497b354a419fa03866f8f44cd
Waoweens <30313089+Waoweens@users.noreply.github.com>
2026-03-13 13:41:53 +0700

    search

---
+++ src/routes/search/+page.server.ts
@@ -0,0 +1,70 @@
+import type { PageServerLoad } from './$types';
+import { db } from '$lib/server/db';
+import { and, desc, eq, sql } from 'drizzle-orm';
+import { products, users } from '$lib/server/db/schema';
+import { error } from '@sveltejs/kit';
+
+export const load: PageServerLoad = async ({ parent }) => {
+	const { query } = await parent();
+
+	if (!query) error(400, 'Query parameter is required');
+
+	const q = query.trim();
+
+	const results = await db.transaction(async (tx) => {
+		await tx.execute(sql`SET LOCAL pg_trgm.similarity_threshold = 0.12;`);
+
+		const titleText = products.title;
+		const descText = sql<string>`coalesce(${products.description}, '')`;
+
+		const titleScore = sql<number>`similarity(${titleText}, ${q})`;
+		const descScore = sql<number>`similarity(${descText}, ${q})`;
+
+		// boost exact contains so "printer" in description always surfaces
+		const exactBoost = sql<number>`
+			(CASE WHEN ${titleText} ILIKE ${'%' + q + '%'} THEN 1.0 ELSE 0 END) +
+			(CASE WHEN ${descText}  ILIKE ${'%' + q + '%'} THEN 0.6 ELSE 0 END)
+    	`;
+
+		const rank = sql<number>`
+			(${exactBoost} * 2.0) + (${titleScore} * 0.7 + ${descScore} * 0.3)
+		`;
+
+		return tx
+			.select({
+				id: products.id,
+				title: products.title,
+				description: products.description,
+				imageUrl: products.imageUrl,
+				price: products.price,
+				createdAt: products.createdAt,
+				created: products.created,
+				rank,
+				user: {
+					id: users.id,
+					username: users.username
+				}
+			})
+			.from(products)
+			.innerJoin(users, eq(products.userId, users.id))
+			.where(
+				and(
+					eq(products.created, true),
+					// IMPORTANT: exact contains OR trigram
+					sql`(
+						${titleText} ILIKE ${'%' + q + '%'}
+						OR ${descText} ILIKE ${'%' + q + '%'}
+						OR ${titleText} % ${q}
+						OR ${descText} % ${q}
+					)`
+				)
+			)
+			.orderBy(desc(rank), desc(products.createdAt));
+	});
+
+	console.log(results);
+
+	return {
+		results
+	};
+};
--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
+	import ProductsList from '$lib/components/ProductsList.svelte';
+
 	let { data } = $props();
 </script>
 
-<p>Searched for {data.query}</p>
+<h1 class="text-lg font-bold">Search: {data.query}</h1>
+<p class="mb-4">{data.results?.length || 0} results found.</p>
+<ProductsList products={data.results} />
